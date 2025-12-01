6a58df07045259fbf7af2d344782e9abc3c4ad34
Luzivam Santos <luzivam.santos28@outlook.com>
2025-11-30 23:33:39 -0300

    criado e modificado os file routes/main_route, implementado com blueprint; file app/__init__.py com as importação das routes e register do blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    # import routes
+    from .routes.main_route import main_bp
+
+    # register routes
+    app.register_blueprint(main_bp)
+
     return app
